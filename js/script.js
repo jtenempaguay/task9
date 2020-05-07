b0eeb7bcd0d06895b34ad6bf3aeab29206d52d55
Juan T <jtenempaguay.com>
2020-05-07 15:51:24 -0400

    Added html and js files

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 jQuery(document).ready(function($) {
-    $('.prepend').on('click', function () {
-        var errors = 0;
-        var words = document.getElementById("words").value;
-        if (words.trim().length == 0){
-            $('#errorMessage').addClass("alert").text("Text Input is Blank!");
-            $('#errorMessage').show();
-            errors = errors +1;
-        } 
-            if (errors==0) {
-            $('#errorMessage').hide();
-            jQuery( '#tasks' ).prepend(( '<p>'+words+'</p>'));
-        }
-    });
+	$('.prepend').on('click', function() {
+		var errors = 0;
+		var words = document.getElementById("words").value;
+		if (words.trim().length === 0) { //Added trim to prevent empty spaces
+			$('#errorMessage').addClass("alert").text("Text Input is Blank!");
+			$('#errorMessage').show();
+			errors = errors + 1;
+		}
+		if (errors === 0) {
+			$('#errorMessage').hide();
+			jQuery('#tasks').prepend(('<p>' + words + '</p>'));
+		}
+	});
 });
